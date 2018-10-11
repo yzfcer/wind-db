92b433c5e9e9c1ffc8cda86f42640657b2d7ff48
yzfcer <yzfcer@163.com>
2018-10-12 00:45:13 +0800

    代码整理

--- src/comman/wind_conv.c
+++ src/comman/wind_conv.c
@@ -22,6 +22,7 @@
 **
 **------------------------------------------------------------------------------------------------------
 *******************************************************************************************************/
+#include "wind_type.h"
 #include "wind_string.h"
 //把字符串转换成带符号整数
 w_bool_t wind_atoi(char *str,w_int32_t *value)
@@ -31,19 +32,19 @@ w_bool_t wind_atoi(char *str,w_int32_t *value)
     w_int32_t num = 0;
     
     sign = (str[0] == '-')?1:0;
-    if(str[i]=='+'||str[i]==' -')
+    if(str[i]=='+'||str[i]=='-')
         i++;
     for(;;i ++)
     {
         if(str[i] <= '9' && str[i] >= '0')
             num = num *10 + str[i] - '0';
         else if(str[i] != 0)
-            return B_FALSE;
+            return W_FALSE;
         else
             break;
     }
     *value = sign?-num:num;
-    return B_TRUE;
+    return W_TRUE;
 }
 
 void wind_itoa(char *str,w_int32_t value)
@@ -76,19 +77,19 @@ w_bool_t wind_atoui(char *str,w_uint32_t *value)
 {
     w_uint32_t num = 0;
     w_uint8_t i = 0;
-    if(str[i]=='+'||str[i]==' -')
+    if(str[i]=='+'||str[i]=='-')
         i++;
     for(;;i ++)
     {
         if(str[i] <= '9' && str[i] >= '0')
             num = num *10 + str[i] - '0';
         else if(str[i] != 0)
-            return B_FALSE;
+            return W_FALSE;
         else
             break;
     }
     *value = num;
-    return B_TRUE;
+    return W_TRUE;
 }
 
 void wind_uitoa(char *str,w_uint32_t value)
@@ -143,13 +144,13 @@ w_bool_t wind_htoi(char *hex,w_uint32_t *value)
     {  
         temp = c2i( *(hex + i));
         if(temp < 0)
-            return B_FALSE;
+            return W_FALSE;
         bits = (len - i - 1) *4;  
         temp = temp << bits;  
         num = num | temp;  
     }
     *value = num;
-    return B_TRUE;  
+    return W_TRUE;  
 }  
 
 void wind_itoh(char *hex,w_uint32_t value)
@@ -171,3 +172,101 @@ void wind_itoh(char *hex,w_uint32_t value)
         *end-- = tmp;  
     }
 }
+
+
+
+
+
+w_bool_t wind_from_uint16(w_uint8_t *arr,w_uint16_t value)
+{
+    int i;
+    for(i = 0;i < 2;i ++)
+    {
+        arr[i] = (w_uint8_t)(value >> (i*8));
+    }
+    return W_TRUE;
+}
+
+w_bool_t wind_from_uint32(w_uint8_t *arr,w_uint32_t value)
+{
+    int i;
+    for(i = 0;i < 4;i ++)
+    {
+        arr[i] = (w_uint8_t)(value >> (i*8));
+    }
+    return W_TRUE;
+}
+
+w_bool_t wind_from_uint64(w_uint8_t *arr,w_uint64_t value)
+{
+    int i;
+    for(i = 0;i < 8;i ++)
+    {
+        arr[i] = (w_uint8_t)(value >> (i*8));
+    }
+    return W_TRUE;
+}
+
+
+w_bool_t wind_from_float32(w_uint8_t *arr,w_fp32_t value)
+{
+    w_uint32_t *pvalue = (w_uint32_t*)&value;
+    return wind_from_uint32(arr,*pvalue);
+}
+
+w_bool_t wind_from_float64(w_uint8_t *arr,w_fp64_t value)
+{
+    w_uint64_t *pvalue = (w_uint64_t*)&value;
+    return wind_from_uint64(arr,*pvalue);
+}
+
+
+w_bool_t wind_to_uint16(w_uint8_t *arr,w_uint16_t *value)
+{
+    int i;
+    w_uint16_t va = 0;
+    for(i = 1;i >= 0;i --)
+    {
+        va = (va * 256) + arr[i];
+    }
+    *value = va;
+    return W_TRUE;
+}
+
+w_bool_t wind_to_uint32(w_uint8_t *arr,w_uint32_t *value)
+{
+    int i;
+    w_uint32_t va = 0;
+    for(i = 3;i >= 0;i --)
+    {
+        va = (va * 256) + arr[i];
+    }
+    *value = va;
+    return W_TRUE;
+}
+
+w_bool_t wind_to_uint64(w_uint8_t *arr,w_uint64_t *value)
+{
+    int i;
+    w_uint64_t va = 0;
+    for(i = 7;i >= 0;i --)
+    {
+        va = (va * 256) + arr[i];
+    }
+    *value = va;
+    return W_TRUE;
+}
+
+w_bool_t wind_to_float32(w_uint8_t *arr,w_fp32_t *value)
+{
+    w_uint32_t *pvalue = (w_uint32_t*)value;
+    return wind_to_uint32(arr,pvalue);
+}
+
+w_bool_t wind_to_float64(w_uint8_t *arr,w_fp64_t *value)
+{
+    w_uint64_t *pvalue = (w_uint64_t*)value;
+    return wind_to_uint64(arr,pvalue);
+}
+
+
